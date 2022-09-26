7443068261e1705e8cf07027ba8653222ec45c03
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-09-26 11:43:44 +0200

    budgetCard component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Button, Stack } from "react-bootstrap"
 import Container from "react-bootstrap/Container";
+import BudgetCard from "./components/BudgetCard";
 
 function App() {
-  return ( 
-    <Container className="my-4">
+  return (
+    <Container className="mb-4">
       <Stack direction="horizontal" gap="2" className="mb-4">
         <h1 className="me-auto">Budgets</h1>
         <Button variant="primary">Add Budget</Button>
@@ -14,9 +15,15 @@ function App() {
           display: "grid", 
           gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", 
           gap: "1rem", 
-          alignItems: "flex-start"
-          }}>
-        
+          alignItems: "flex-start",
+        }} 
+      >
+        <BudgetCard 
+          name="Entertainment" 
+          gray
+          amount={600} 
+          max={1000}>
+        </BudgetCard>        
       </div>
     </Container>
   )
