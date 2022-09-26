618416d6a888f9f89115a1eae41e972b634bca58
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-09-26 17:19:50 +0200

    budget context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Button, Stack } from "react-bootstrap"
+import { Button, Stack } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import BudgetCard from "./components/BudgetCard";
 
 function App() {
   return (
-    <Container className="mb-4">
+    <Container className="my-4">
       <Stack direction="horizontal" gap="2" className="mb-4">
         <h1 className="me-auto">Budgets</h1>
         <Button variant="primary">Add Budget</Button>
@@ -21,7 +21,7 @@ function App() {
         <BudgetCard 
           name="Entertainment" 
           gray
-          amount={600} 
+          amount={1200} 
           max={1000}>
         </BudgetCard>        
       </div>
